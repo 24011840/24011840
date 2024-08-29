2e8bc914c88851e86af58a8125150e6882df41c5
24011840 <juliaaveiropaiva@gmail.com>
2024-08-29 02:27:42 -0300

    Update LojaDeConveniencia.c

--- exercícios/LojaDeConveniencia.c
+++ exercícios/LojaDeConveniencia.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main() {
+    char nomeProduto[50];
+    int quantidade;
+    float precoUnitario, valorTotal, valorTotalDia = 0;
+    char opcao;
+
+    do {
+        printf("Digite o nome do produto: ");
+        scanf("%s", nomeProduto);
+        printf("Digite a quantidade vendida: ");
+        scanf("%i", &quantidade);
+        printf("Digite o preco unitario do produto: ");
+        scanf("%f", &precoUnitario);
+
+    valorTotal = quantidade * precoUnitario;
+        printf("Valor total da venda: R$ %.2f\n", valorTotal);
+
+    valorTotalDia += valorTotal;
+
+        printf("Deseja registrar outra venda? (s/n): ");
+        scanf(" %c", &opcao);
+    } while (opcao == 's' || opcao == 'S');
+
+    printf("Valor total arrecadado no dia: R$ %.2f\n", valorTotalDia);
+
+    return 0;
+}
